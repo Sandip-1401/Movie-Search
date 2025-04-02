8fdaa6e6d61588919e48c6d854a4bdc0b4fe6697
Sandip 1401 <sandipsongra14@gmail.com>
2025-04-03 02:52:25 +0530

    Today work on Search Functionality

--- JS/app.js
+++ JS/app.js
@@ -1,12 +1,8 @@
 import API_KEY from './config.js';
 
-
-const imgs = document.querySelector(".imgs");
-const allImg = document.querySelector(".allImg");
-
-
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
 
 
 const getTrendingMovies = async () => {
@@ -16,6 +12,9 @@ const getTrendingMovies = async () => {
    let data = await response.json();
    const movies = data.results;
    console.log(movies);
+
+   const imgs = document.querySelector(".imgs");
+   const allImg = document.querySelector(".allImg");
 
    movies.forEach((movie) => {
       
@@ -44,6 +43,31 @@ const getTrendingMovies = async () => {
 const getTrend = () => {
    getTrendingMovies();
 }
+// getTrend();
+
+const SearchBtn = document.querySelector(".SearchBtn");
+const searchMovie = async () => {
+   const SearchInp = document.querySelector(".SearchInp");
+   let movieName = SearchInp.value;
+   let URLSEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${movieName}`;
+   let response = await fetch(URLSEARCH);
+   let data = await response.json();
+   const movies = data.results;
+   console.log(movies);
+   
+   
+
+   data.forEach((movie) => {
+      let URLIMG = `https://image.tmdb.org/t/p/w500${movies.poster_path}`;
+      let imgs = document.createElement("img");
+      imgs.src = URLIMG;
+   });
+};
+
+SearchBtn.addEventListener("click", () => {
+   searchMovie();
+});
+
 
 
 // const getMovies = async () => {
@@ -67,3 +91,85 @@ const getTrend = () => {
 // const get = () => {
 //    getMovies();
 // }
+
+
+function updateConnectionStatus(){
+   if(navigator.onLine){
+      if(document.querySelector("#offline-div")){
+         const alertDiv = document.createElement("div");
+         alertDiv.innerHTML = "You are back Online...";
+         alertDiv.id = `online-div`;
+         alertDiv.style.cssText = 
+         `
+         position: fixed;
+         top: 0; left: 0; right: 0;
+         background-color: #4CAF50;
+         color: white;
+         font-size: 18px;
+         padding: 10px;
+         text-align: center;
+         z-index: 1000;
+         transition: opacity 5s ease-in-out;
+         `;
+
+         if(document.querySelector("#offline-div")){
+            document.querySelector("#offline-div").remove();
+         };
+         document.body.appendChild(alertDiv);
+
+         setTimeout(() => {
+            const onlineDiv = document.querySelector("#online-div");
+            if(onlineDiv){
+               onlineDiv.remove();
+            }
+         }, 3000)
+      }
+   }else{
+      if(!document.querySelector("#offline-div")){
+         const alertDiv = document.createElement("div");
+         alertDiv.id = "offline-div";
+         alertDiv.innerHTML = `You are offline. Please check yout network...!`;
+         alertDiv.style.cssText = `
+         position: fixed;
+         top: 0; left: 0; right: 0;
+         background-color: #f44336;
+         color: white;
+         text-align: center;
+         padding: 10px;
+         z-index: 1000;
+         font-size: 18px;
+         transition: opacity 5s ease-in-out;
+         `;
+         document.body.appendChild(alertDiv);
+      }
+     
+   }
+}
+updateConnectionStatus();
+window.addEventListener("online", updateConnectionStatus);
+window.addEventListener("offline", updateConnectionStatus);
+
+const microphone = document.querySelector(".microphone");
+const SearchInp = document.querySelector(".SearchInp");
+const audio = new Audio("Images/google.mp3");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if(SpeechRecognition){
+   const recognition = new SpeechRecognition();
+   recognition.lang = 'en-US';
+   recognition.interinResults = false;
+   recognition.contionuous = false;
+
+   microphone.addEventListener("click", () => {
+      audio.play();
+      SearchInp.value = "";
+      recognition.start();
+   });
+     
+   recognition.onresult = ((event) => {
+      const transcript = event.results[0][0].transcript;
+      SearchInp.value = transcript;
+   });
+
+}
